02338a7c035a8bbcd0387813a02644d385dd0def
Niels Swinkels <bitbucket@nielsswinkels.nl>
2016-11-21 15:09:26 +0100

    Bigger textfield

--- resources/assets/js/components/AddTextButton.vue
+++ resources/assets/js/components/AddTextButton.vue
@@ -6,11 +6,12 @@
             <button >{{button}}</button>
         </span>
         <form v-if="editmode" v-bind:action="action" method="POST">
-            <input type="text" name="text" v-bind:value="text" />
+            <textarea name="text" v-bind:rows="rows" v-bind:cols="cols">{{ text }}</textarea> 
             <input type="hidden" name="type" v-bind:value="type" />
             <input type="hidden" name="parent_type" v-bind:value="parent_type" />
             <input type="text" name="language" v-bind:value="language" size="2"/>
             <input type="submit" name="submit" value="Add"/>
+            <input type="reset" value="Cancel" v-on:click="editmode = !editmode"/>
             <input type="hidden" name="_token" v-bind:value="csrf_token">
             <input type="hidden" name="_method" v-bind:value="method">
             <input type="hidden" name="parent_id" v-bind:value="parent_id">
@@ -28,6 +29,35 @@
         mounted() {
             console.log('Component ready.')
         },
-        props: ['button', 'type', 'text', 'language', 'parent_id', 'parent_type', 'action', 'csrf_token', 'method']
+        props: {
+            button: {
+                type: String,
+                default: 'Add text'
+            },
+            text: {
+                type: String,
+                default: ''
+            },
+            type: {
+                type: String,
+                default: 'title'
+            },
+            language: {
+                type: String,
+                default: 'en'
+            },
+            rows: {
+                type: String,
+                default: '1'
+            },
+            cols: {
+                type: String,
+                default: '50'
+            },
+            parent_id: String,
+            action: String,
+            method: String,
+            csrf_token: String
+        }
     }
 </script>

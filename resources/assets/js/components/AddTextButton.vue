01e90006c45d618ddf0ead882a3e64ace53e3504
nielsswinkels <niels.sw@gmail.com>
2017-03-06 14:42:12 +0100

    fix default language to sv

--- resources/assets/js/components/AddTextButton.vue
+++ resources/assets/js/components/AddTextButton.vue
@@ -43,7 +43,7 @@
             },
             language: {
                 type: String,
-                default: 'en'
+                default: 'sv'
             },
             rows: {
                 type: String,

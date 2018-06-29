e305accb5ce4bf9dcd51a3b8e903e3f538b1c8bf
nielsswinkels <niels.sw@gmail.com>
2018-06-29 15:06:11 +0200

    Undo-ing the ugly hack that disabled registration of new accounts

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,8 +62,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        // FIXME very ugly hack to close registration for now
-        return;
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
